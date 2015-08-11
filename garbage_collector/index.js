e78a977b126bf8b2b0b70f205887d23f00c7c503
wangyan <wyvernnot@gmail.com>
2015-08-11 16:07:49 +0800

    everything is now put in front end

--- garbage_collector/index.js
+++ garbage_collector/index.js
@@ -5,7 +5,12 @@ var wss = new WebSocketServer({port: 3002});
 var readline = require('readline');
 var parseLine = require('./lib/parseLine.js');
 var start = Date.now();
+var _ = require('underscore');
 
+/**
+ * headlessly broad cast an object to every connected clients
+ * @param obj
+ */
 function broadCast(obj) {
   wss.clients.forEach(function (client) {
     client.send(JSON.stringify(obj));
@@ -17,52 +22,15 @@ var rl = readline.createInterface({
   terminal: false
 });
 
-process.stdin.pipe(process.stdout)
-
 rl.on('line', function (l) {
-  var pased = parseLine(l);
-  if (pased && pased.pid) {
-
-    var emit = [];
-
-
-
-
-    for (var i in pased) {
-
-      if (typeof pased[i] === 'number') {
-
-        if ([
-            "total_size_after",
-            "allocated",
-            "holes_size_before",
-            "holes_size_after",
-            "mark",
-            "sweep",
-            "stepscount",
-            "stepstook"
-          ].indexOf(i) > -1) {
-          emit.push({
-            y: pased[i],
-            group: i
-          });
-        }
-
-
-      }
-
+    var pased = parseLine(l);
+    if (pased && pased.pid) {
+      pased.time = start + pased.time;
+      broadCast(pased);
     }
-
-    emit.push({
-      y: pased.total_size_before - pased.total_size_after,
-      group: "total_size_diff"
-    })
-
-
-    emit.forEach(function (item) {
-      item.x = start + pased.time;
-    });
-    broadCast(emit);
   }
-});
+);
+
+// just pipe everything through after dealing with lines
+process.stdin.pipe(process.stdout);
 
